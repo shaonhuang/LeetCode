db301c861449c26c522326950520e65f3c7d1bf5
shaonhuang <yukuaihuang@icloud.com>
2020-11-26 20:45:24 +0800

    question 27 upload

---
+++ 6.z-字形变换.py
@@ -0,0 +1,13 @@
+#
+# @lc app=leetcode.cn id=6 lang=python3
+#
+# [6] Z 字形变换
+#
+
+# @lc code=start
+class Solution:
+    def convert(self, s: str, numRows: int) -> str:
+        numColumns = len(s)
+        dataMatrix =
+# @lc code=end
+
